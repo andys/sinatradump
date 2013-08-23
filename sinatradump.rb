946d38169e66cbaeecae9fbf5bee09697d409f7d
Andrew Snow <andrew@modulus.org>
2013-08-23 12:00:56 +1000

    format in simple HTML

--- sinatradump.rb
+++ sinatradump.rb
@@ -3,11 +3,12 @@ require 'sinatra'
 
 [:get, :put, :post, :delete, :options, :patch].each do |verb|
   send(verb, //) do
-    content_type 'text/plain'
-    "--------- ENV ---------\n" +
-      (ENV.map {|k,v| "#{k}: #{v}\n"}.join("")) + 
-      "\n\n\n--------- request.env ---------\n" +
-      (request.env.map {|k,v| "#{k}: #{v}\n"}.join(""))
+    content_type 'text/html'
+    "<H1>ENV</H1><table border='1'>\n" +
+      (ENV.keys.sort.map {|k| "<tr><th>#{k}</th><td>#{ENV[k]}</td></tr>\n"}.join("")) + 
+      "</table><p><h1>request.env</h1><table border='1'>\n" +
+      (request.env.keys.sort.map {|k| "<tr><th>#{k}</th><td>#{request.env[k]}</td></tr>\n"}.join("")) + "</table>"
+      
   end
 end 
   
